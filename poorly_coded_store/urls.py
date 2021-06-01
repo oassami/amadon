b42de056f9f8369f5b7b390e22684acb1c821bdc
oassami <76979230+oassami@users.noreply.github.com>
2021-05-31 21:18:04 -0700

    Fixed all issues

--- poorly_coded_store/urls.py
+++ poorly_coded_store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('checkout/', views.checkout)
+    path('checking_out/<int:product_id>', views.checking_out),
+    path('checkout/<int:order_id>', views.checkout),
 ]
--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,16 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db.models import Sum
 from .models import Order, Product
 
 def index(request):
     context = {
-        "all_products": Product.objects.all()
+        "all_products": Product.objects.all(),
+        "all_orders": Order.objects.all(),
     }
     return render(request, "store/index.html", context)
 
-def checkout(request):
+def checking_out(request, product_id):
+    product=Product.objects.get(id=product_id)
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
+    total_charge = quantity_from_form * product.price
     print("Charging credit card...")
-    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    order=Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+    return redirect(f'/checkout/{order.id}')
+
+def checkout(request, order_id):
+    # last_order = Order.objects.get(id=order_id)
+    context={}
+    sums=Order.objects.aggregate(Sum('quantity_ordered'), Sum('total_price'))
+    context['qty_so_far']= str(sums['quantity_ordered__sum'])
+    context['total_so_far']= str(round(sums['total_price__sum'],2))
+    context['last_order'] = str(Order.objects.get(id=order_id).total_price)
+    return render(request, "store/checkout.html", context)
